ce1f262591faf9aa136aefd48bc0e265fe9400f1
Thanh Pham <khanhthy@yahoo.com>
2025-02-09 21:46:55 -0800

    Improve Category componrent

--- app/components/Categories.tsx
+++ app/components/Categories.tsx
@@ -1,6 +1,45 @@
+import Image from "next/image"
+
 const Categories = () => {
     return (
-        <p>Categories</p>
+        <div className="pt-3 cursor-pointer pb-6 flex items-center space-x-12">
+            <div className="pb-4 flex flex-col items-center space-y-2 border-white border-b-2 opacity-80 hover:border-gray-200 hover:opacity-100">
+                <Image
+                    src="/icn_category_beach.jpeg"
+                    alt="Category- Beach"
+                    width={20}
+                    height={20}
+                />
+                <span className="text-xs">Beach</span>
+            </div>
+            <div className="pb-4 flex flex-col items-center space-y-2 border-white border-b-2 opacity-80 hover:border-gray-200 hover:opacity-100">
+                <Image
+                    src="/icn_category_beach.jpeg"
+                    alt="Category- Beach"
+                    width={20}
+                    height={20}
+                />
+                <span className="text-xs">Vilas</span>
+            </div>
+            <div className="pb-4 flex flex-col items-center space-y-2 border-white border-b-2 opacity-80 hover:border-gray-200 hover:opacity-100">
+                <Image
+                    src="/icn_category_beach.jpeg"
+                    alt="Category- Beach"
+                    width={20}
+                    height={20}
+                />
+                <span className="text-xs">Cabins</span>
+            </div>
+            <div className="pb-4 flex flex-col items-center space-y-2 border-white border-b-2 opacity-80 hover:border-gray-200 hover:opacity-100">
+                <Image
+                    src="/icn_category_beach.jpeg"
+                    alt="Category- Beach"
+                    width={20}
+                    height={20}
+                />
+                <span className="text-xs">Tiny Home</span>
+            </div>
+        </div>
     )
 }
  
--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,7 @@ import Categories from "./components/Categories";
 
 export default function Home() {
   return (
-      <main className="">
+      <main className="max-w-[1500px] mx-auto px-6">
         <Categories />
       </main>
   )}
-
