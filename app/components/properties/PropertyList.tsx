23c7c0dc47d538a26ceebef2c0dbbdd39468c71f
Thanh Pham <khanhthy@yahoo.com>
2025-02-10 12:52:09 -0800

    Add folder property with 2 components PropertyList and PropertyListItem

---
+++ app/components/properties/PropertyList.tsx
@@ -0,0 +1,15 @@
+import PropertyListItem from "./PropertyListItem"
+const PropertyList = () => {
+    return (
+        <>
+            <PropertyListItem />
+            <PropertyListItem />
+            <PropertyListItem />
+            <PropertyListItem />
+            <PropertyListItem />
+            <PropertyListItem />
+        </>
+    )
+}
+
+export default PropertyList
