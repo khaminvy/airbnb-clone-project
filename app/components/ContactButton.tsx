27e867c54cedac5fe293e6f0641d93b79b101aa2
Thanh Pham <khanhthy@yahoo.com>
2025-02-16 19:56:57 -0800

    Create LandlordDetailPage on url lanlords and ContactButton

---
+++ app/components/ContactButton.tsx
@@ -0,0 +1,7 @@
+const ContactButton = () => {
+    return (
+        <div className="mt-6 cursor-pointer py-4 px-5 bg-airbnb hover:bg-airbnb-dark transition text-white rounded-xl">Contact</div>
+    )
+}
+
+export default ContactButton
