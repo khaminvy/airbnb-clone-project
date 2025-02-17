f9e17b0b74fe1112f497f5a3df413c645e4786cd
Thanh Pham <khanhthy@yahoo.com>
2025-02-16 18:55:19 -0800

    update ReservationSidebar component

--- app/components/properties/ReservationSidebar.tsx
+++ app/components/properties/ReservationSidebar.tsx
@@ -3,6 +3,7 @@ const ReservationSidebar = () => {
         <aside className=" mt-6 p-6 col-span-2 rounded-xl border border-gray-300 shadow-sm ">
             <h2 className="mb-5 text-2xl">$200 per night</h2>
             <div className="mb-6 p-3 border border-gray-400 rounded-xl">
+                <label className="mb-2 block font-bold text-xs">Guests</label>
                 <select name="" id="" className="w-full ml-1 text-xm">
                     <option value="">1</option>
                     <option value="">2</option>
@@ -10,6 +11,21 @@ const ReservationSidebar = () => {
                     <option value="">4</option>
                 </select>
             </div>
+            <div className="w-full mb-6 py-6 text-center text-white bg-airbnb hover:bg-airbnb-dark rounded-xl">Book</div>
+            <div className="mb-4 flex justify-between align-middle">
+                <p>$200 * 4 nights</p>
+                <p>$800</p>
+            </div>
+            <div className="mb-4 flex justify-between align-middle">
+                <p>Djangobnb fee</p>
+                <p>$40</p>
+            </div>
+            <hr />
+            <div className="mt-4 flex justify-between align-middle font-bold">
+                <p>Total</p>
+                <p>$840</p>
+            </div>
+            <hr />
         </aside>
     )
 }
--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -14,6 +14,7 @@ const PropertyDetailPage = () => {
              </div>
              <div className="grid grid-cols-1 md:grid-cols-5 gap-4">
                 <div className="py-6 pr-6 col-span-3">
+                   
                     <h1 className="mb-4 text-4xl">Property name</h1>
                     <span className="mb-6 block text-lg text-gray-500">
                         4 guests - 2 bedrooms - 1 bathroom
